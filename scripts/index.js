2b8c37ffd72bed5b084e14b86e25d731e6337dfe
jsantiag <jsantiag@wellesley.edu>
2018-10-02 14:10:24 -0500

    index.js single DOM read fn

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,5 @@
 'use strict';
 /* global Store, Api, $ */
-
-
-
-/**
- * @function decorateResponse
- * Uses Youtube API response to create an array of "decorated" video objects as 
- * defined at the top of the file.
- * @param   {object} response - should match Youtube API response shape
- * @returns {array}
- */
-// TASK:
-// 1. Map through the response object's `items` array
-// 2. Return an array of objects, where each object contains the keys `id`, `title`, 
-//    `thumbnail` which each hold the appropriate values from the API item object. You 
-//    WILL have to dig into several nested properties!
-//
-// TEST IT! Grab an example API response and send it into the function - make sure
-// you get back the object you want.
-const decorateResponse = function(response) {
-  const decorated = response.items.map((item) => {
-    return {
-      id: item.id.videoId,
-      title: item.snippet.title,
-      thumbnail: item.snippet.thumbnails.default.url
-    };
-  });
-  return decorated;
-};
-
-
 // When DOM is ready:
 $(function () {
   // TASK:
